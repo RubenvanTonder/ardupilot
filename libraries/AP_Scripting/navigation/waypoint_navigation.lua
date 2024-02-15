e2ba1dd28891582f01cbe8428a7fd07ab1950e39
RubenvanTonder <rubenvtonder@gmail.com>
2024-02-15 12:05:02 +0200

    Added Guidance Axis to Navigation

--- libraries/AP_Scripting/navigation/waypoint_navigation.lua
+++ libraries/AP_Scripting/navigation/waypoint_navigation.lua
@@ -5,10 +5,8 @@ local dest_position = {}
 dest_position.x = 50.0
 dest_position.y = 50.0
 
--- current position
-local current_position = {}
-current_position.x = 0.0
-current_position.y = 0.0
+-- current location of the sailboat
+local current_location
 
 -- Source poition
 local source_position = {}
@@ -32,29 +30,8 @@ waypoint.numberof = 0
 waypoint.home = 0
 waypoint.mission = 0
 
-local vector3 
-vector3 =Vector3f()
-
--- Constants Used to convert Lat Lng Alt to NED
-local wgs84_e = 0.08181919084
-local wgs84_a = 6378137.0
-
--- Convert Latitude, Longitude and Altitude to North, East and Down
-function convert_LLA_to_NED(lat, lon, alt)
-    local d = wgs84_e * math.sin(lat)
-    local n = wgs84_a / math.sqrt(1 - d*d)
-
-    local ned = Vector3f()
-    ned.x((n + alt) * math.cos(lat) * math.cos(lon))
-    ned.y(((n + alt)* math.cos(lat) * math.sin(lon)))
-    ned.z(((1 - wgs84_e*wgs84_e)*n + alt) * math.sin(lat))
-    print(ned:x())
-    return ned
-end
-
-
 -- Generate a Location
-function location(mission)
+local function location(mission)
     local loc  = Location()
     loc:lat(math.floor(mission:x()))
     loc:lng(math.floor(mission:y()))
@@ -62,61 +39,48 @@ function location(mission)
     return loc
 end
 
-
-
 -- Load the waypoints
-function load_waypoints()
-
-    -- Get total number of waypoints
-    waypoint.numberof = mission:num_commands()-1
-    print("Numberof " .. waypoint.numberof)
-    waypoint.home = mission:get_item(0)
-    waypoint.mission = mission:get_item(1)
-    --print(waypoint.mission:x())
-    waypoint.home = location(waypoint.home)
-    waypoint.mission = location(waypoint.mission)
-    
-    vector3 = waypoint.home:get_distance_NED(waypoint.mission)
-    print("X: " .. vector3:x() .. "Y: " .. vector3:y())
-    print("waypoint x: " .. waypoint.home:lat())
-    vector3 = convert_LLA_to_NED(waypoint.home:lat(), waypoint.home:lng(), waypoint.home:alt())
-    print("X: " .. vector3:x() .. "Y: " .. vector3:y())
+local function load_waypoints()
+     -- Get total number of waypoints
+     waypoint.numberof = mission:num_commands()-1
+     --print("Numberof " .. waypoint.numberof)
+     waypoint.home = mission:get_item(0)
+     waypoint.mission = mission:get_item(1)
+     --print(waypoint.mission:x())
+     waypoint.home = location(waypoint.home)
+     waypoint.mission = location(waypoint.mission)
 end
 
-function update()
+local function bearing_and_length_to_waypoint(dest, src)
+    local dest_src = src.get_distance_NE(dest)
+    track_heading_angle = math.atan(dest_src:y(),dest_src:x())
+    l_track = math.sqrt(dest_src:x()^2 + dest_src:y())
+end
 
+function UPDATE()
+
+    -- Wait for ahrs to initialize
     if not ahrs:initialised() then
-        return update, 1000
+        return UPDATE, 1000
     end
 
-    -- Get Current Coordinates
-    local position = ahrs:get_relative_position_NED_origin()
-    ahrs:get_origin()
+    -- Wait for sailboat to be armed
+    if arming:is_armed() then
 
-    if position ~= nil then
-        -- Initialize Values
-        current_position.x = position:x()
-        current_position.y = position:y()
-
-
-        local y_diff_dest = (dest_position.y - current_position.y)
-        local x_diff_dest = (dest_position.x - current_position.x)
-        track_heading_angle = math.atan(y_diff_dest,x_diff_dest)
-        print("Track Heading Angle: " .. track_heading_angle)
-
-        l_track = math.sqrt((x_diff_dest*x_diff_dest) + (y_diff_dest*y_diff_dest))
-        print("Track Distance: " .. l_track)
-
-        local y_diff = (current_position.y - source_position.y)
-        local x_diff = (current_position.x - source_position.x)
-
-        guidance_axis.s = math.cos(track_heading_angle) * x_diff + math.sin(track_heading_angle) * y_diff
-        print("s: " .. guidance_axis.s)
-
-        guidance_axis.e = -math.sin(track_heading_angle) * x_diff + math.cos(track_heading_angle) * y_diff
-        print("e: " .. guidance_axis.e)
-        
+        -- Load in the waypoints used for navigation including home
         load_waypoints()
+       
+        -- Get the Current Location of the sailboat 
+        -- Home will only stay fixed when sailboat is armed
+        current_location = ahrs:get_location()
+
+        -- Get distance to and angle between home and waypoint 1
+        bearing_and_length_to_waypoint(waypoint.mission, waypoint.home)
+
+        print("Track Heading " .. track_heading_angle)
+
+        print("Track Distance " .. l_track)
+
     end
     return update, 1000
 end
