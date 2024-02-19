e5e7c8b4b9ff0bf1a33da90307b83a7104ee3e79
Ruben <rubevtonder@gmail.com>
2024-02-19 13:41:08 +0200

    Added Guidance Axis and Waypoint Loading into Array

--- Tools/autotest/scripts/waypoint_navigation.lua
+++ Tools/autotest/scripts/waypoint_navigation.lua
@@ -1,17 +1,7 @@
--- Script for navigating between waypoint
-
--- Destination position
-local dest_position = {}
-dest_position.x = 50.0
-dest_position.y = 50.0
+-- Script for navigating between waypoints
 
 -- current location of the sailboat
 local current_location
-
--- Source poition
-local source_position = {}
-source_position.x = 0.0
-source_position.y = 0.0
 
 -- track heading angle
 local track_heading_angle = 0
@@ -26,9 +16,11 @@ guidance_axis.e = 0.0
 
 -- Waypoints
 local waypoint = {}
-waypoint.numberof = 0
-waypoint.home = 0
-waypoint.mission = 0
+waypoint.total = 0
+waypoint.mission = {Location(), Location()}
+local waypoint_reached = 0
+local current_waypoint = 0
+local loaded = false
 
 -- Generate a Location
 local function location(mission)
@@ -41,48 +33,73 @@ end
 
 -- Load the waypoints
 local function load_waypoints()
-     -- Get total number of waypoints
-     waypoint.numberof = mission:num_commands()-1
-     --print("Numberof " .. waypoint.numberof)
-     waypoint.home = mission:get_item(0)
-     waypoint.mission = mission:get_item(1)
-     --print(waypoint.mission:x())
-     waypoint.home = location(waypoint.home)
-     waypoint.mission = location(waypoint.mission)
+    -- Get total number of waypoints
+    waypoint.total = mission:num_commands()-1
+    print("Total waypoints " .. waypoint.total)
+    -- Iterate through waypoints
+    for i=0, (waypoint.total) do
+        waypoint.mission[i] = location(mission:get_item(i))
+    end
 end
 
 local function bearing_and_length_to_waypoint(dest, src)
-    local dest_src = src.get_distance_NE(dest)
+    local dest_src = src:get_distance_NE(dest)
     track_heading_angle = math.atan(dest_src:y(),dest_src:x())
-    l_track = math.sqrt(dest_src:x()^2 + dest_src:y())
+    l_track = math.sqrt(dest_src:x()^2 + dest_src:y()^2)
 end
+
+local function guidance_axis_calc(current, src)
+    local current_src = src:get_distance_NE(current)
+    print(current_src:x() .. " " .. current_src:y())
+    guidance_axis.s = math.cos(track_heading_angle) * current_src:x() + math.sin(track_heading_angle) * current_src:y()
+    guidance_axis.e = -math.sin(track_heading_angle) * current_src:x() + math.cos(track_heading_angle) * current_src:y()
+end
+
+
+
 
 function UPDATE()
 
     -- Wait for ahrs to initialize
     if not ahrs:initialised() then
         return UPDATE, 1000
-    end
+    end 
+
+   
+
 
     -- Wait for sailboat to be armed
     if arming:is_armed() then
+        
+        if not loaded then
+            load_waypoints()
+            loaded = true
+        end
 
-        -- Load in the waypoints used for navigation including home
-        load_waypoints()
-       
-        -- Get the Current Location of the sailboat 
-        -- Home will only stay fixed when sailboat is armed
-        current_location = ahrs:get_location()
+        if (current_waypoint < waypoint.total) then
+            -- Get the Current Location of the sailboat 
+            -- Home will only stay fixed when sailboat is armed
+            current_location = ahrs:get_location()
+            --print("Current Location " .. current_location)
+            -- Get distance to and angle between source waypoint and destination waypoint
+            -- waypoint.mission[current_waypoint] is the source waypoint
+            -- waypoint.mission[current_waypoint] is the destination waypoint
 
-        -- Get distance to and angle between home and waypoint 1
-        bearing_and_length_to_waypoint(waypoint.mission, waypoint.home)
-
-        print("Track Heading " .. track_heading_angle)
-
-        print("Track Distance " .. l_track)
-
+            -- Calculate the bearing and length between source and destination waypoint
+            bearing_and_length_to_waypoint(waypoint.mission[current_waypoint+1], waypoint.mission[current_waypoint])
+            --print(waypoint.mission[0]:lat() .. " " .. waypoint.mission[0]:lng() .. " " ..waypoint.mission[0]:alt())
+            --print("\n")
+            --print(waypoint.mission[1]:lat() .. " " .. waypoint.mission[1]:lng() .. " " .. waypoint.mission[1]:alt())
+            -- Get distance along the track and cross-track error between home and waypoint 1
+            guidance_axis_calc(current_location, waypoint.mission[current_waypoint])
+            
+            print("Track Heading " .. track_heading_angle)
+            print("Track Distance " .. l_track)
+            print("Track Travelled " .. guidance_axis.s)
+            print("Cross-track  " .. guidance_axis.e)
+        end
     end
-    return update, 1000
+    return UPDATE, 1000
 end
 
-return update()
+return UPDATE()
