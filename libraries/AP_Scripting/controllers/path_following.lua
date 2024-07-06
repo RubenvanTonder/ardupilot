cdb75502167c156f0ba0e6f7751388c860782785
RubenvanTonder <rubenvtonder@gmail.com>
2024-07-06 12:39:50 +0200

    Path following logs updated

--- libraries/AP_Scripting/controllers/path_following.lua
+++ libraries/AP_Scripting/controllers/path_following.lua
@@ -175,6 +175,8 @@ local function update()
         x_r = constrain(math.atan(PTH_PI.update(e:get()),1), -0.5, 0.5)
         x_d = x_p:get() - x_r - beta
         x_d = x_d 
+        -- Log path following controller data
+        PTH_PI.log("PTHC")
       else 
         x_d = x_p:get() - beta
         --print("Desired Tack Heading " .. x_d)
@@ -184,9 +186,8 @@ local function update()
         gcs:send_text(6, string.format('Desired heading set failed'))
       end
 
-    -- Log path following controller data
-    PTH_PI.log("PTHC")
-    logger:write("BXY",'Beta,BodyX,BodyY,GlobalX,GlobalY','fffff',tostring(beta),tostring(vbx),tostring(vby),tostring(vex),tostring(vey))
+    
+    logger:write("BXY",'Beta,BodyX,BodyY,GlobalX,GlobalY,Xd','fffff',tostring(beta),tostring(vbx),tostring(vby),tostring(vex),tostring(vey),tostring(x_d))
    end
 end
  
